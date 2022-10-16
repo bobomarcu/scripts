585b8369f488590d2446fa61c3fbf84d433647c4
Bogdan Marcu <bmarcu@yonder.local>
2022-10-16 19:37:18 +0300

    first commit

---
+++ scanLinux.sh
@@ -0,0 +1,43 @@
+
+echo "PC Scanner";
+echo "================================";
+
+echo "SCAN STARTED >>";
+
+function pauza () {
+	x=1
+	while [ $x -le 3 ]
+	do
+		sleep 1
+		echo "*"
+		x=$(( $x + 1 ))
+
+	done
+}
+echo "================================";
+echo "CPU";
+cpu="$( lscpu | grep 'Model name'| awk '{print $3 " " $4 " " $5 " " $8}' )";
+pauza
+echo "================================"; 
+echo "MEM";
+mem="$( lsmem | grep 'Total online memory' | awk '{print $4}' )";
+pauza
+echo "================================";
+echo "IP:";
+host="$( hostname -i | awk '{print $2}' )";
+pauza
+echo "================================";
+echo "USER";
+user="$( whoami )";
+pauza
+echo "================================";
+echo "GPU:";
+gpu="$( sudo lshw -C display | grep 'product' | cut -d ':' -f 2 )";
+pauza
+echo "================================";
+echo "DONE ...";
+data="$(date | awk '{print $2 " " $3 " " $7}')"
+
+curl -X POST -d "user=$user&mem=$mem&cpu=$cpu&host=$host&gpu=$gpu&os="Linux"&data=$data" http://192.168.100.52/pc-data;
+
+printf "\n";
---
+++ scanWindows.bash
@@ -0,0 +1,27 @@
+user="$( whoami )"
+
+N=4
+mem1="$(systeminfo | findstr 'Total' | awk -v N=$N '{print $N}' )"
+
+cpu="$( wmic cpu get name )"
+
+gpu="$(wmic path win32_VideoController get name)"
+
+ip1="$(ipconfig | findstr  "v4" | awk -v N=14 '{print $N}')"
+
+echo $user
+
+echo $mem1
+
+echo $cpu
+
+echo $gpu
+
+ip2="$(echo $ip1 | awk -v N=2 '{print $N}')"
+
+echo $ip2
+
+data="$( date | awk '{print $3 " " $2 " " $6}')"
+
+echo $data
+ curl -X POST -d "user=$user&mem=$mem1&cpu=$cpu&host=$ip2&gpu=$gpu&os="Windows"&data=$data" http://192.168.100.52/pc-data;
